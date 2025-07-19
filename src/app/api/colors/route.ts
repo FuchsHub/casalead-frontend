6724307fb6355b42d75623cd495de3ff0dcccca2
FuchsHub <luisnothardt99@gmail.com>
2025-07-19 13:49:02 +0200

    wertana

--- src/app/api/colors/route.ts
+++ src/app/api/colors/route.ts
@@ -3,9 +3,10 @@ import { NextResponse } from 'next/server'
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY! // Wichtig: SERVICE ROLE nur, wenn RLS gesetzt ist!
-)
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_ANON_KEY!
+);
+
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url)
